7a35ced49260e6771927011631ad6d7c3513577c
ARXChrono <chronoxp@hotmail.com>
2023-03-18 18:05:36 +0800

    add google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,11 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>If the build is successful images will be rendered 👇</h1>
+        <h1>The Daily News</h1>
         {articles.map((article) => (
           <>
             <p>{JSON.stringify(article)}</p>
             <img height="200" src={`images/${article.image}`} />
-            <hr />
           </>
         ))}
       </div>
